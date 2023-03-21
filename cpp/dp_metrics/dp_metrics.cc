0dc63a1df1717d10ce5036edb0439205087e0429
Akshay Pundle <akshaypundle@gmail.com>
2023-03-21 00:51:28 -0700

    added some cpu and memory stats

--- cpp/dp_metrics/dp_metrics.cc
+++ cpp/dp_metrics/dp_metrics.cc
@@ -1,6 +1,67 @@
-#include <stdio.h>
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "sys/sysinfo.h"
+#include "sys/times.h"
+#include "sys/types.h"
+#include <iostream>
+#include <unistd.h>
+#include <iostream>
+#include <cstdlib>
+
+static unsigned long long lastTotalUser, lastTotalUserLow, lastTotalSys, lastTotalIdle;
+
+void init(){
+    FILE* file = fopen("/proc/stat", "r");
+    fscanf(file, "cpu %llu %llu %llu %llu", &lastTotalUser, &lastTotalUserLow,
+        &lastTotalSys, &lastTotalIdle);
+    fclose(file);
+}
+
+double getCurrentValue(){
+    double percent;
+    FILE* file;
+    unsigned long long totalUser, totalUserLow, totalSys, totalIdle, total;
+
+    file = fopen("/proc/stat", "r");
+    fscanf(file, "cpu %llu %llu %llu %llu", &totalUser, &totalUserLow,
+        &totalSys, &totalIdle);
+    fclose(file);
+
+    if (totalUser < lastTotalUser || totalUserLow < lastTotalUserLow ||
+        totalSys < lastTotalSys || totalIdle < lastTotalIdle){
+        //Overflow detection. Just skip this value.
+        percent = -1.0;
+    }
+    else{
+        total = (totalUser - lastTotalUser) + (totalUserLow - lastTotalUserLow) +
+            (totalSys - lastTotalSys);
+        percent = total;
+        total += (totalIdle - lastTotalIdle);
+        percent /= total;
+        percent *= 100;
+    }
+
+    lastTotalUser = totalUser;
+    lastTotalUserLow = totalUserLow;
+    lastTotalSys = totalSys;
+    lastTotalIdle = totalIdle;
+
+    return percent;
+}
 
 int main(void) {
-  printf("Hello World!\n");
+
+  struct sysinfo memInfo;
+  sysinfo (&memInfo);
+  init();
+  long long physMemUsed = memInfo.totalram - memInfo.freeram;
+  //Multiply in next statement to avoid int overflow on right hand side...
+  physMemUsed *= memInfo.mem_unit;
+
+  for(int i=0;i<10000000;i++) {
+    std::cout << physMemUsed << " " << getCurrentValue()<<"\n";
+    sleep(1);
+  }
   return 0;
 }
